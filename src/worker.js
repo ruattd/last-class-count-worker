92e7dc1a4426311afca868c60638b17a79526ffa
任天天 <ruattqwq@gmail.com>
2025-04-17 11:54:11 +0800

    Complete basic functions

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,4 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-// const dayjs = require("./day.js")();
+import { DateTime } from "luxon";
 
 let map = {
   "Y": "语文",
@@ -19,12 +9,13 @@ let map = {
   "Z": "组装",
   "P": "PS",
   "T": "体育",
-}
+  " ": "自习",
+};
 
 let courses = [
   {
-    start_date: "2025-04-14 CST",
-    end_date: "2025-04-30 CST",
+    start_date: DateTime.fromISO("2025-04-14T00:00:00.000+08:00"),
+    end_date: DateTime.fromISO("2025-04-30T23:59:59.999+08:00"),
     list: [
       "CCYYEZT DPP",
       "YDEE PPMMCC",
@@ -35,16 +26,83 @@ let courses = [
       "ZZ"
     ]
   }
-]
+];
 
-function next() {
+let times = [
+  "08:30", "09:20", "10:20", "11:10", "11:50",
+  "15:10", "16:00", "17:10", "17:50",
+  "19:40", "21:10"
+];
 
+function getNow() { return DateTime.now().setZone("Asia/Shanghai"); }
+
+function getCourseArray(datetime, startIndex) {
+  let currentPeriod = courses.find(item => datetime >= item.start_date && datetime <= item.end_date);
+  if (!currentPeriod) return null;
+  let dayDiff = datetime.diff(currentPeriod.start_date, "days").toObject().days;
+  let index = Math.floor(dayDiff) % currentPeriod.list.length;
+  let course = currentPeriod.list[index];
+  let array = [];
+  for (let i = startIndex ? startIndex : 0; i < course.length; i++) {
+    array.push(course[i]);
+  }
+  return array;
 }
 
-// let response = `datetime ${dayjs.format()}`;
+function toCourseTable(courseArray, currentIndex) {
+  let table = [];
+  let index = 0;
+  courseArray.forEach(item => {
+    let name = map[item];
+    if (currentIndex === index) name = "[" + name + "]";
+    table.push(name);
+    index++;
+  });
+  return table.join(" ");
+}
+
+function generate() {
+  let now = getNow();
+  let todayCourse = getCourseArray(now);
+  let currentTimeIndex = times.findIndex(item => DateTime.fromFormat(item, "hh:mm", { zone: "Asia/Shanghai" }) > now);
+  let lastCourseCount = times.length - currentTimeIndex;
+  
+  let countMap = {};
+  Object.keys(map).forEach(key => countMap[key] = 0);
+  let countingDate = now;
+  let countingCourse = getCourseArray(now, currentTimeIndex);
+  while (true) {
+    do {
+      countingCourse.forEach(item => countMap[item]++);
+      countingDate = countingDate.plus({ days: 1 });
+      countingCourse = getCourseArray(countingDate);
+    } while(countingCourse);
+    let nextPeriod = courses.find(course => course.start_date >= countingDate);
+    if (!nextPeriod) break;
+    countingDate = nextPeriod.start_date;
+  }
+
+  let countTextArray = [];
+  Object.keys(map).forEach(key => countTextArray.push(`${map[key]}:\t${countMap[key]}`));
+  
+  let text =
+`NOW: ${now.toFormat("EEEE, MM/dd hh:mm:ss, yyyy")}
+
+TODAY CLASSES
+-------------
+${toCourseTable(todayCourse, currentTimeIndex)}
+${lastCourseCount} class${lastCourseCount > 1 ? "es" : ""} left today.
+
+LAST COUNT
+----------
+${countTextArray.join("\n")}
+(by ${countingDate.plus({ days: -1 }).toFormat("MM/dd, yyyy")})
+`;
+  return text;
+}
 
 export default {
   async fetch(request, env, ctx) {
-    return new Response(response);
+    return new Response(generate());
   },
 };

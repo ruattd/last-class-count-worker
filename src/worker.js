f6eeb4016c9bec3cf0a8f049913fa758071f4f76
任天天 <ruattqwq@gmail.com>
2025-04-17 15:05:43 +0800

    Fix some bugs

--- src/worker.js
+++ src/worker.js
@@ -10,20 +10,30 @@ let map = {
   "P": "PS",
   "T": "体育",
   " ": "自习",
+  "0": "NUL",
 };
+
+function getDate(text) {
+  return DateTime.fromFormat(text, "yyyy-MM-dd", { zone: "Asia/Shanghai" });
+}
 
 let courses = [
   {
-    start_date: DateTime.fromISO("2025-04-14T00:00:00.000+08:00"),
-    end_date: DateTime.fromISO("2025-04-30T23:59:59.999+08:00"),
+    start_date: getDate("2025-04-28"),
+    end_date: getDate("2025-04-30"),
+    list: [ "00000000000" ]
+  },
+  {
+    start_date: getDate("2025-04-14"),
+    end_date: getDate("2025-05-01"),
     list: [
       "CCYYEZT DPP",
       "YDEE PPMMCC",
       "CCEEMZZPPYY",
       "DYCC TMZMDD",
       "YYCE MM  EE",
-      "DYMM MDD ",
-      "ZZ"
+      "DYMM MDD 00",
+      "00000000 ZZ"
     ]
   }
 ];
@@ -34,7 +44,9 @@ let times = [
   "19:40", "21:10"
 ];
 
-function getNow() { return DateTime.now().setZone("Asia/Shanghai"); }
+function getNow() {
+  return DateTime.now().setZone("Asia/Shanghai");
+}
 
 function getCourseArray(datetime, startIndex) {
   let currentPeriod = courses.find(item => datetime >= item.start_date && datetime <= item.end_date);
@@ -51,12 +63,13 @@ function getCourseArray(datetime, startIndex) {
 
 function toCourseTable(courseArray, currentIndex) {
   let table = [];
-  let index = 0;
+  let index = -1;
   courseArray.forEach(item => {
+    index++;
+    if (item === "0") return;
     let name = map[item];
     if (currentIndex === index) name = "[" + name + "]";
     table.push(name);
-    index++;
   });
   return table.join(" ");
 }
@@ -64,33 +77,37 @@ function toCourseTable(courseArray, currentIndex) {
 function generate() {
   let now = getNow();
   let todayCourse = getCourseArray(now);
-  let currentTimeIndex = times.findIndex(item => DateTime.fromFormat(item, "hh:mm", { zone: "Asia/Shanghai" }) > now);
-  let lastCourseCount = times.length - currentTimeIndex;
+  let currentTimeIndex = times.findIndex(item => DateTime.fromFormat(item, "HH:mm", { zone: "Asia/Shanghai" }) > now);
+  let lastCourseCount = todayCourse ? times.length - currentTimeIndex : 0;
   
   let countMap = {};
   Object.keys(map).forEach(key => countMap[key] = 0);
   let countingDate = now;
   let countingCourse = getCourseArray(now, currentTimeIndex);
   while (true) {
-    do {
+    while (countingCourse) {
       countingCourse.forEach(item => countMap[item]++);
       countingDate = countingDate.plus({ days: 1 });
       countingCourse = getCourseArray(countingDate);
-    } while(countingCourse);
+    }
     let nextPeriod = courses.find(course => course.start_date >= countingDate);
     if (!nextPeriod) break;
     countingDate = nextPeriod.start_date;
+    countingCourse = getCourseArray(countingDate);
   }
 
+  delete countMap["0"];
   let countTextArray = [];
-  Object.keys(map).forEach(key => countTextArray.push(`${map[key]}:\t${countMap[key]}`));
-  
+  Object.keys(map).forEach(key => {
+    if (key !== "0") countTextArray.push(`${map[key]}:\t${countMap[key]}`)
+  });
+
   let text =
-`NOW: ${now.toFormat("EEEE, MM/dd hh:mm:ss, yyyy")}
+`NOW: ${now.toFormat("EEEE, MM/dd HH:mm:ss, yyyy")}
 
 TODAY CLASSES
 -------------
-${toCourseTable(todayCourse, currentTimeIndex)}
+${todayCourse ? toCourseTable(todayCourse, currentTimeIndex) : "Nothing"}
 ${lastCourseCount} class${lastCourseCount > 1 ? "es" : ""} left today.
 
 LAST COUNT

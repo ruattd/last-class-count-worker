cca3ca1b68b7291cb1bb518bbf5a26767b1b9fb7
任天天 <ruattqwq@gmail.com>
2025-04-19 07:20:30 +0800

    Complete HTML page

--- static/index.js
+++ static/index.js
@@ -5,6 +5,7 @@ const TIME_FORMAT = "HH:mm";
 const datetime = document.getElementById("datetime");
 const day = document.getElementById("day");
 const table = document.getElementById("table");
+const left_today = document.getElementById("left-today");
 const left = document.getElementById("left");
 
 const DateTime = luxon.DateTime;
@@ -23,34 +24,54 @@ setInterval(() => {
 }, 200);
 
 async function onUpdate(data, times) {
+    left_today.innerHTML = data.today_left_classes;
     day.innerHTML = data.today_is_passed ? "明日" : "今日";
     let courseTableArray = [];
     let index = -1;
     data.course_table.forEach(item => {
         index++;
         if (item === "NUL") return;
-        let start = DateTime.fromISO(times.start_times[index]).toFormat(TIME_FORMAT);
-        let end = DateTime.fromISO(times.end_times[index]).toFormat(TIME_FORMAT);
+        let start = times.start_times[index].toFormat(TIME_FORMAT);
+        let end = times.end_times[index].toFormat(TIME_FORMAT);
         let text = `${start}-${end} ${item}`;
-        if (!data.today_is_passed && data.current_class_index === index)
-            text = `<b>${text}</b>`
+        if (!data.today_is_passed && data.current_class_index === index) {
+            text = `<span id="current" class="current next">${text}</span>`
+            const start_next = times.start_times[index];
+            if (start_next) {
+                const updater = setInterval(() => {
+                    if (now > start_next) {
+                        document.getElementById("current").className = "current";
+                        clearInterval(updater);
+                    }
+                }, 1000);
+            }
+        } else {
+            text = `<span>${text}</span>`;
+        }
         courseTableArray.push(text);
     });
-    courseTableArray.push(`今天还有 ${data.today_left_classes} 节课`);
     table.innerHTML = courseTableArray.join("<br />");
+    let leftClassesArray = [];
+    Object.keys(data.left_count).forEach(key => {
+        let text = `<tr><td>${key}</td><td class="number">${data.left_count[key]}</td></tr>`;
+        leftClassesArray.push(text);
+    })
+    left.innerHTML = `<table><tbody>${leftClassesArray.join("")}</tbody></table>`;
 }
 
+console.log("Fetching times");
 fetch(`${API_ADDR}/times`).then(response => {
     response.json().then(times => {
+        for (let i = 0; i < times.start_times.length && i < times.end_times.length; i++) {
+            times.start_times[i] = DateTime.fromISO(times.start_times[i]);
+            times.end_times[i] = DateTime.fromISO(times.end_times[i]);
+        }
         setInterval(() => {
-            let currentIndex = times.end_times.findIndex(time => {
-                const t = DateTime.fromISO(time);
-                return now > t;
-            });
+            let currentIndex = times.end_times.findIndex(time => now > time);
             if (currentIndex <= lastRequestIndex) return;
             lastRequestIndex = currentIndex;
-            onUpdate(times);
             fetch(`${API_ADDR}`).then(response => response.json().then(data => onUpdate(data, times)));
+            console.log("Triggered update");
         }, 1000);
     });
 });
